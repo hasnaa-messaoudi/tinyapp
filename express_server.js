53791d39b69d0cf910321194587e5a1b51a0213b
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-09-01 04:48:54 +0000

    add body-parser module to stingfy the body response of post method

--- express_server.js
+++ express_server.js
@@ -7,7 +7,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+};
+
+console.log(generateRandomString());
+
 app.set("view engine", "ejs")
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,10 +40,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

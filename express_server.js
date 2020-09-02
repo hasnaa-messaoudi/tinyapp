d3cee8f2071fb3b779cc4334406390bd25daf659
hasnaa messaoudi <hasnaa.messaoudi@gmail.com>
2020-09-02 05:19:54 +0000

    start implementing cookies

--- express_server.js
+++ express_server.js
@@ -16,11 +16,18 @@ app.set("view engine", "ejs")
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser())
+
 // when it receives a POST request to /urls it responds with a redirection to /urls/:shortURL, where shortURL is the random string we generated.
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = { 
+    username: req.cookies["username"],
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL] 
+  };
   if (urlDatabase[shortURL]) {
     res.render("urls_show", templateVars);
   } else {
@@ -30,11 +37,12 @@ app.post("/urls", (req, res) => {
 });
 
 //Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
-
 app.post("/urls/:shortURL/delete", (req, res) => {
-  //urlDatabase[shortURL] = req.params.shortURL;
   delete urlDatabase[req.params.shortURL];
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
   
   
@@ -42,11 +50,32 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 // Add a POST route that updates a URL resource; POST /urls/:id
 app.post("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
   res.render("urls_show", templateVars);
 });
 
+// Add an endpoint to handle a POST to /login in your Express server. 
+app.post("/login", (req, res) => {
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls : urlDatabase
+  };
+  res.cookie('username', req.body.username);
+  res.redirect("urls");
+});
 
+
+app.post("/logout", (req, res) => {
+  let templateVars = { 
+    urls : urlDatabase
+  };
+  res.clearCookie('username');
+  res.redirect("urls");
+});
 
 // the requests to the endpoint "/u/:shortURL" will redirect to its longURL
 app.get("/u/:shortURL", (req, res) => {
@@ -71,16 +100,28 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  console.log('Cookies: ', req.cookies)
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
+  
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { 
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
